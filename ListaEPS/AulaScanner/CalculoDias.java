400ae5de1b2c982f21f6940f94bd7fdcaeeac60a
Igor Oliveira Cerqueira <igorcerqueira.ic30042008@gmail.com>
2025-10-10 08:48:15 -0300

    Update CalculoDias.java

--- ListaEPS/AulaScanner/CalculoDias.java
+++ ListaEPS/AulaScanner/CalculoDias.java
@@ -4,11 +4,8 @@ public class CalculoDias {
 	public static void main(String args[]) {
 		Scanner ler = new Scanner(System.in);
 		int dias,mes,anos;
-		
-		System.out.println("-----------------------------");
-		System.out.println("| SOU O GÊNIO DO ALGORITIMO |");
-		System.out.println("-----------------------------");
-		System.out.println("Vou calcular os dias");
+
+		System.out.println("Vamos calcular o mes e o ano pelos dias");
 		System.out.println("Informe os dias: ");
 		dias = ler.nextInt();
 		mes = dias/30;
@@ -18,3 +15,4 @@ public class CalculoDias {
 	}
 
 }
+
